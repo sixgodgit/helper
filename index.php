a0bf5d116b1c30daa7974932a44a0a47199dd2d4
sixgodgit <75098232+sixgodgit@users.noreply.github.com>
2022-02-11 01:12:44 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+    {
+   "name":"hvh.expert",
+   "description":"enjoy free helper data",
+   "update_timestamp": 1641936404,
+   "url_format": "https://raw.githubusercontent.com/sixgodgit/hvh.expert/main/maps/%map%.json"
+   }
